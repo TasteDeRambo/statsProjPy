d09d961afe7387ade94c08399e0521815805a873
TasteDeRambo <anonimous3746@gmail.com>
2023-12-18 19:57:47 -0600

    close to final product

--- zipChart.py
+++ zipChart.py
@@ -1,44 +1,64 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Read the CSV file
-df = pd.read_csv('Police_Arrests.csv')
+df = pd.read_csv('Police_Arrests.csv', dtype={30: str, 45: str, 46: str})
 
-# Filter out rows where 'AgeAtArrestTime' is empty
+# Filter out rows with empty 'AgeAtArrestTime'
 df = df[df['AgeAtArrestTime'].notna()]
 
-# Find the mode of 'arLZip'
-mostArrestZipCode = df['arLZip'].mode()[0]
+# Group by 'ArLZip' and get the mode, mean of 'AgeAtArrestTime'
+zipCodeAges = df.groupby('ArLZip')['AgeAtArrestTime'].agg(['mean', lambda x: x.mode().iloc[0]])
 
-# Filter the DataFrame for the most arrested zip code
-df_most_arrest = df[df['arLZip'] == mostArrestZipCode]
+# Find the 'ArLZip' with the most arrests
+mostArrestZipCode = df['ArLZip'].mode().iloc[0]
+
+# Get 'AgeAtArrestTime' for the 'ArLZip' with the most arrests
+arrestAgesInZip = df[df['ArLZip'] == mostArrestZipCode]['AgeAtArrestTime']
 
 # Plot histogram
-plt.hist(df_most_arrest['arLZip'], bins=10, edgecolor='black')
-plt.title('Histogram of Most Arrested Zip Code')
-plt.show()
-
-# Plot relative frequency histogram
-plt.hist(df_most_arrest['arLZip'], bins=10, edgecolor='black', density=True)
-plt.title('Relative Frequency Histogram of Most Arrested Zip Code')
-plt.show()
-
-# Plot cumulative frequency histogram
-plt.hist(df_most_arrest['arLZip'], bins=10, edgecolor='black', cumulative=True)
-plt.title('Cumulative Frequency Histogram of Most Arrested Zip Code')
+plt.figure(figsize=(12, 8))
+plt.hist(arrestAgesInZip, bins=20, edgecolor='black')
+plt.title('Histogram of Arrest Ages in Zip Code ' + str(mostArrestZipCode))
+plt.xlabel('Age')
+plt.ylabel('Frequency')
 plt.show()
 
 # Plot box plot
-plt.boxplot(df_most_arrest['arLZip'])
-plt.title('Box Plot of Most Arrested Zip Code')
-plt.show()
-
-# Plot bar graph
-df_most_arrest['arLZip'].value_counts().plot(kind='bar')
-plt.title('Bar Graph of Most Arrested Zip Code')
+plt.figure(figsize=(12, 8))
+plt.boxplot(arrestAgesInZip)
+plt.title('Box Plot of Arrest Ages in Zip Code ' + str(mostArrestZipCode))
+plt.ylabel('Age')
 plt.show()
 
 # Plot pie chart
-df_most_arrest['arLZip'].value_counts().plot(kind='pie', autopct='%1.1f%%')
-plt.title('Pie Chart of Most Arrested Zip Code')
+plt.figure(figsize=(12, 8))
+arrestAgesInZip.value_counts().plot(kind='pie', autopct='%1.1f%%')
+plt.title('Pie Chart of Arrest Ages in Zip Code ' + str(mostArrestZipCode))
+plt.ylabel('')
+plt.show()
+
+# Plot cumulative frequency
+plt.figure(figsize=(12, 8))
+plt.hist(arrestAgesInZip, bins=20, edgecolor='black', cumulative=True)
+plt.title('Cumulative Frequency of Arrest Ages in Zip Code ' + str(mostArrestZipCode))
+plt.xlabel('Age')
+plt.ylabel('Cumulative Frequency')
+plt.show()
+
+# Plot relative frequency
+plt.figure(figsize=(12, 8))
+plt.hist(arrestAgesInZip, bins=20, edgecolor='black', weights=np.ones(len(arrestAgesInZip)) / len(arrestAgesInZip))
+plt.title('Relative Frequency of Arrest Ages in Zip Code ' + str(mostArrestZipCode))
+plt.xlabel('Age')
+plt.ylabel('Relative Frequency')
+plt.show()
+
+# Plot bar graph
+plt.figure(figsize=(12, 8))
+arrestAgesInZip.value_counts().plot(kind='bar')
+plt.title('Bar Graph of Arrest Ages in Zip Code ' + str(mostArrestZipCode))
+plt.xlabel('Age')
+plt.ylabel('Frequency')
 plt.show()
